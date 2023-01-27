ea8bfdf3fea8acf7c155ac3e49fff92411a485eb
jobala <japhethobalak@gmail.com>
2023-01-27 22:23:01 +0000

    chore: add slack as canvas

--- canvas/slack.go
+++ canvas/slack.go
@@ -1,10 +1,5 @@
 package canvas
 
-import (
-	"fmt"
-	"net/http"
-)
-
 type SlackCanvas struct {
 	channel string
 }
@@ -18,14 +13,6 @@ func Slack() *SlackCanvas {
 const SLACK_TOKEN = ""
 
 func (s *SlackCanvas) Draw(art, title, artist string) error {
-	img, _ := http.Get(art)
-	req, _ := http.NewRequest(http.MethodPost, "https://slack.com/api/files.upload", img.Body)
-
-	req.Header.Set("Content-Type", "multipart/form-data")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", SLACK_TOKEN))
-
-	client := http.Client{}
-	client.Do(req)
-
+	// upload to slack
 	return nil
 }
